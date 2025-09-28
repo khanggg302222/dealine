f39f15b831b133fdf7b82d47ff2ebc8aa6080f85
khanggg302222 <khannnng302@gmail.com>
2025-09-28 21:08:41 +0700

    Update repository.py

--- repository.py
+++ repository.py
@@ -18,8 +18,9 @@ def delete_user_by_id(user_id: str):
     return users_collection.delete_one({"_id": ObjectId(user_id)})
 
 def get_user_by_id(user_id):
-    # kiểm tra kiểu dữ liệu rồi xử lý
+    # check the data and then process it
     if isinstance(user_id, str):
         return users_collection.find_one({"_id": ObjectId(user_id)})
     else:
         return users_collection.find_one({"_id": user_id})
+
