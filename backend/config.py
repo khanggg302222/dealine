14cebaa50eeaf5b05157eb1e9eeff9690fad8349
khanggg302222 <khannnng302@gmail.com>
2025-10-15 21:00:30 +0700

    Delete backend directory

--- backend/config.py
+++
@@ -1,5 +0,0 @@
-# connect to MongoDB
-from pymongo import MongoClient
-connect_mongo = MongoClient("mongodb://localhost:27017/")
-db = connect_mongo["user_db"]
-users_collection = db["users"]
--- backend/schema.py
+++
@@ -1,9 +0,0 @@
-#output 
-# Format MongoDB data to return to client
-def format_user(user_raw) -> dict:
-    return {
-        "id": str(user_raw["_id"]),   
-        "name": user_raw["name"],
-        "email": user_raw["email"],
-        "age": user_raw["age"]
-    }
